c6ebace9e8c48b1cbe4f884780431d994826dc3e
VietnamecDruhy <maggotsvk@gmail.com>
2024-04-09 15:19:35 +0200

    php bootstrap all-plants

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,1 @@
-import './bootstrap';
+import "bootstrap/dist/js/bootstrap.bundle";
--- resources/views/all-plants.blade.php
+++ resources/views/all-plants.blade.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>All Plants</title>
+
     @vite(['resources/css/templates.css', 'resources/css/all-plants.css', 'resources/js/app.js'])
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- FontAwesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
@@ -530,10 +529,5 @@
                     </div>
                 </div>
             </footer>
-
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-                crossorigin="anonymous"></script>
 </body>
-
 </html>
