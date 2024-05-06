296450ec9393a8e19d88108c460d8131d178c551
peterhubina <peter.hubina1@gmail.com>
2024-05-06 13:49:50 +0200

    Update docs, remove remember token

--- database/migrations/2024_04_08_000002_create_users_table.php
+++ database/migrations/2024_04_08_000002_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->enum('role', ['User', 'Admin'])->default('User');
             $table->timestamps();
         });
